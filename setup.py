3be322dd37ec4370fd825a4bd0ebec3eafb2b2e7
Abir Hossain <84902162+ABIRHOSSAIN10@users.noreply.github.com>
2021-11-04 16:13:50 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='instaclon',  
+     name='instacloner',  
      version='0.1',
      scripts=['instacloner'] ,
      author="ABIR HOSSAIN",
